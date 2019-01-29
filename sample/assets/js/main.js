dea3a96304933c616c51176eed40d6e9a07ab3b2
bdeshak <43699326+bdeshak@users.noreply.github.com>
2019-01-29 23:14:56 +0600

    Add files via upload

---
+++ sample/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+ 
+   var $ = skrollr.init({
+   	render: function(data){
+     //Debugging - log the current scroll position.
+     //console.log(data.curTop);
+   	}
+   });
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
